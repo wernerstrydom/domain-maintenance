a41635bca23d536f45e14b53dba038dfe688bfce
Werner Strydom <bloudraak@users.noreply.github.com>
2023-01-01 12:51:07 -0800

    Discovers Registered Domains in Amazon Route53

---
+++ src/DomainMaintenance.Functions/DiscoverRegisteredDomains.cs
@@ -0,0 +1,112 @@
+using System;
+using Azure.Data.Tables;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Table;
+using Amazon.Route53;
+using Amazon.Route53.Model;
+using Amazon.Route53Domains;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.Immutable;
+using Amazon.Route53Domains.Model;
+
+namespace DomainMaintenance.Functions
+{
+    public class DiscoverRegisteredDomains
+    {
+        [FunctionName("DiscoverRegisteredDomains")]
+
+        public async Task Run(
+            [TimerTrigger("*/30 * * * * *")] TimerInfo timer,
+            [Table("domains")] TableClient table,
+            CancellationToken cancellationToken,
+            ILogger log)
+        {
+            var registered = await DiscoverDomains(cancellationToken);
+
+            // we're using the table as a cache to know which domains we processed before
+            var cached = await table.QueryAsync<RegisteredDomain>(maxPerPage: 1000).ToDictionaryAsync(d => d.DomainName).ConfigureAwait(false);
+
+            List<string> added = new(), deleted = new(), updated = new();
+            foreach (var (domainName, domain) in registered)
+            {
+                if (domain.Expiry.AddDays(31) < DateTime.UtcNow)
+                {
+                    log.LogInformation($"{domainName} has expired and should be deleted");
+                    deleted.Add(domainName);
+                }
+                else if (cached.ContainsKey(domainName))
+                {
+                    log.LogInformation($"{domainName} exists in cache");
+                    if (cached[domainName] != registered[domainName])
+                    {
+                        // exists in cache, and should be updated
+                        log.LogInformation($"{domainName} needs to be updated in cache");
+                        updated.Add(domainName);
+                    }
+                }
+                else
+                {
+                    // needs to be added
+                    log.LogInformation($"{domainName} does not exists in cache");
+                    added.Add(domainName);
+                }
+            }
+
+            foreach (var (domainName, domain) in cached)
+            {
+                if (!registered.ContainsKey(domainName))
+                {
+                    log.LogInformation($"{domainName} exists in cache but not in registrar and should be deleted");
+                    deleted.Add(domainName);
+                }
+            }
+
+            foreach (var domainName in added)
+            {
+                var domain = registered[domainName];
+                await table.AddEntityAsync(domain, cancellationToken);
+            }
+
+            foreach (var domainName in deleted)
+            {
+                var domain = registered[domainName];
+                await table.DeleteEntityAsync(domain.PartitionKey, domain.RowKey, default, cancellationToken);
+            }
+
+            foreach (var domainName in updated)
+            {
+                var other = cached[domainName];
+                var domain = registered[domainName];
+                await table.UpdateEntityAsync(domain, Azure.ETag.All);
+            }
+        }
+
+
+        private static async Task<Dictionary<string, RegisteredDomain>> DiscoverDomains(CancellationToken cancellationToken)
+        {
+            var domains = new Dictionary<string, RegisteredDomain>();
+            var client = new AmazonRoute53DomainsClient();
+
+            ListDomainsResponse response;
+            ListDomainsRequest request = new ListDomainsRequest { MaxItems = 10 };
+
+            do
+            {
+                response = await client.ListDomainsAsync(request, cancellationToken);
+                foreach (var domain in response.Domains)
+                {
+                    RegisteredDomain d = new RegisteredDomain(domain.DomainName, domain.Expiry, domain.AutoRenew, domain.TransferLock);
+                    domains.Add(d.DomainName, d);
+                }
+                request.Marker = response.NextPageMarker;
+            } while (!string.IsNullOrEmpty(response.NextPageMarker));
+
+            return domains;
+        }
+    }
+}
---
+++ src/DomainMaintenance.Functions/RegisteredDomain.cs
@@ -0,0 +1,90 @@
+﻿//
+// RegisteredDomain.cs
+//
+// Author:
+//       Werner Strydom <hello@wernerstrydom.com>
+//
+// Copyright (c) 2023 Copyright (c) 2023 Werner Strydom
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using Azure;
+using Azure.Data.Tables;
+
+namespace DomainMaintenance.Functions
+{
+    public class RegisteredDomain : ITableEntity, IEquatable<RegisteredDomain>
+    {
+        public RegisteredDomain(string domainName, DateTime expiry, bool autoRenew, bool transferLock)
+        {
+            PartitionKey = "RegisteredDomain";
+            DomainName = domainName;
+            RowKey = domainName;
+            Expiry = expiry;
+            AutoRenew = autoRenew;
+            TransferLock = transferLock;
+        }
+
+        public RegisteredDomain()
+        {
+        }
+
+        public string DomainName { get; set; }
+        public DateTime Expiry { get; set; }
+        public bool AutoRenew { get; set; }
+        public bool TransferLock { get; set; }
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp{ get; set; }
+        public ETag ETag { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as RegisteredDomain);
+        }
+
+        public bool Equals(RegisteredDomain other)
+        {
+            return other is not null &&
+                   DomainName == other.DomainName &&
+                   Expiry == other.Expiry &&
+                   AutoRenew == other.AutoRenew &&
+                   TransferLock == other.TransferLock;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(DomainName, Expiry, AutoRenew, TransferLock);
+        }
+
+        public static bool operator ==(RegisteredDomain left, RegisteredDomain right)
+        {
+            return EqualityComparer<RegisteredDomain>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(RegisteredDomain left, RegisteredDomain right)
+        {
+            return !(left == right);
+        }
+    }
+
+}
+
